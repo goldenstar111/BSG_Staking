cdfa673035eec72bd8bbfd040e3e54e5ab5e3229
goldenstar111 <yatsukalbert@outlook.com>
2022-11-09 06:34:35 +0000

    contract info

---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+export const CHAIN_ID = parseInt(process.env.REACT_APP_CHAIN_ID);
+export const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
+export const CONTRACT_ADDRESS_USDT = process.env.REACT_APP_USDT_ADDRESS;
